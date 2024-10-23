43701117f3da50e1625c471cefc0e82222eb30cc
mohammaduzzal <aalp72142@gmail.com>
2024-10-23 11:24:29 +0600

    added banner section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,23 @@
 const Banner = () => {
     return (
         <div>
-            Banner
+           <div
+  className="hero h-full md:h-[35rem] mb-10 rounded-2xl"
+  style={{
+    backgroundImage: "url(https://i.ibb.co.com/xH2WykR/cover.png)",
+  }}>
+  <div className="hero-overlay bg-opacity-10"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-3 text-xl md:text-3xl font-bold">Discover an exceptional cooking class tailored for you!</h1>
+      <p className="mb-3">
+      Cooking is the art of transforming raw ingredients into delicious meals through creativity and technique. It brings people together, nourishing both the body and soul with flavors and shared experiences.
+      </p>
+      <button className="btn btn-primary rounded-4xl bg-emerald-400">Explore Now</button>
+      <button className="btn btn-primary bg-transparent rounded-4xl">Our Feedback</button>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
