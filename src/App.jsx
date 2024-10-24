83a7db6f1e5a4bf86abfd1cf01e51bc2f9d878af
mohammaduzzal <aalp72142@gmail.com>
2024-10-24 09:23:11 +0600

    complete this webside with the final touch of calculate time and calories

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,39 @@ import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   const [recipeQueue, setRecipeQueue]  = useState([])
+  const [cooking, setCooking] = useState([])
+  const [totalTime, setTotalTime] = useState(0)
+  const [totalCalories, setTotalCalories] = useState(0)
 
   const handleAddRecipeQueue = recipe =>{
-    console.log('mama to ka lagse bare');
-    setRecipeQueue([...recipeQueue, recipe])
+    // console.log('mama to ka lagse bare');
+    const isExist = recipeQueue.find(previousRecipe => previousRecipe.recipe_id === recipe.recipe_id)
+    if(!isExist){
+      setRecipeQueue([...recipeQueue, recipe])
+    }
+    else{
+      alert('Recipe already added')
+    }
   }
+
+  const handleRemoveRecipe = id =>{
+    // console.log('toka marse re',id);
+    //find which  recipe to remove from table 1
+    const removedRecipe = recipeQueue.find(recipe => recipe.recipe_id === id)
+    //remove from table 1
+    const updatedQueue = recipeQueue.filter(recipe => recipe.recipe_id !== id)
+    setRecipeQueue(updatedQueue)
+    setCooking([...cooking,removedRecipe])
+
+  }
+
+  const calculateTC = (time, calories) =>{
+    setTotalTime(totalTime + time)
+    setTotalCalories(totalCalories + calories)
+  }
+
+
+
   // console.log(recipeQueue);
 
   return (
@@ -29,7 +57,13 @@ function App() {
             <Recipes handleAddRecipeQueue={handleAddRecipeQueue}/>
 
             {/* sidebar */}
-            <Sidebar recipeQueue={recipeQueue}/>
+            <Sidebar recipeQueue={recipeQueue} 
+            handleRemoveRecipe={handleRemoveRecipe} 
+            calculateTC={calculateTC}
+            totalTime={totalTime}
+            totalCalories=
+            {totalCalories}
+            cooking={cooking}/>
            </section>
     
     </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@
 
 import PropTypes from 'prop-types';
 
-const Sidebar = ({ recipeQueue}) => {
+const Sidebar = ({ recipeQueue, handleRemoveRecipe,cooking,calculateTC, totalTime,totalCalories}) => {
     return (
         <div className="md:w-1/3 border-2 rounded-lg">
             {/* 1st part */}
@@ -23,12 +23,14 @@ const Sidebar = ({ recipeQueue}) => {
      
      {
         recipeQueue.map((recipe, idx) => 
-        <tr className="hover" key={idx}>
+        <tr className="hover text-gray-500" key={idx}>
           <th>{idx + 1}</th>
           <td>{recipe.recipe_name}</td>
           <td>{recipe.preparing_time}</td>
           <td>{recipe.calories}</td>
-          <td>{<button className='btn bg-emerald-400 rounded-full hover:bg-transparent text-gray-600'>Preparing</button>}</td>
+          <td>{<button onClick ={()=> {handleRemoveRecipe(recipe.recipe_id)
+            calculateTC(recipe.preparing_time,recipe.calories)}
+          } className='btn bg-emerald-400 rounded-full hover:bg-transparent text-gray-600'>Preparing</button>}</td>
         </tr>)
      }
      
@@ -37,13 +39,52 @@ const Sidebar = ({ recipeQueue}) => {
 </div>
 
 {/* 2nd part */}
+<div className="overflow-x-auto">
+        <h2 className='border-b-2 text-center text-xl font-bold py-3'>cooking on process : {cooking.length}</h2>
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Time</th>
+        <th>Calories</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+     
+     {
+       cooking.map((recipe, idx) => 
+        <tr className="hover text-gray-500" key={idx}>
+          <th>{idx + 1}</th>
+          <td>{recipe.recipe_name}</td>
+          <td>{recipe.preparing_time}</td>
+          <td>{recipe.calories}</td>
+        
+        </tr>)}
+     <tr className='text-gray-500 border-none'>
+     <td></td>
+     <td></td>
+     <td>Total Time = {totalTime}</td>
+     <td>Total Calories = {totalCalories}</td>
+     </tr>
+     
+    </tbody>
+  </table>
+</div>
 
         </div>
     );
 };
 
 Sidebar.propTypes = {
-    recipeQueue: PropTypes.array
+    recipeQueue: PropTypes.array,
+    cooking:PropTypes.array,
+    handleRemoveRecipe:PropTypes.func,
+    calculateTC:PropTypes.func,
+    totalTime:PropTypes.number,
+    totalCalories:PropTypes.number
     
 };
 
